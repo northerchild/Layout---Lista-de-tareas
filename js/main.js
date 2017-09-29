37f585d3f23d3be692cce38e192637e83b4ad00b
Northerchild <osarus-13@hotmail.com>
2017-09-29 12:30:18 -0500

    Se soluciona el bug de las tareas

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@
     };
     var comprobarInput = function(){
         tareaInput.className = "";
-        teareaInput.setAttribute("placeholder", "Agrega tus tareas");
+        teareaInput.setAttribute("placeholder", "Agrega tu tarea");
     };
  
     var eleminarTarea = function(){
